6fc0366cc38e56639e24583f22b66d0af968d7be
duhcy <35508017+duhcy@users.noreply.github.com>
2025-04-27 00:18:18 -0400

    Update epvptrade.js

--- commands/core/epvptrade.js
+++ commands/core/epvptrade.js
@@ -4,7 +4,7 @@ module.exports = {
     name: 'epvptrade',
     description: "Instructions for an Epvp trade.",
 
-    async execute({ client, interaction, messageFlag }) {
-        interaction.reply({content: `Follow these steps:\n\nSelect a post from the above list\nSend a trade request, and then leave a positive comment on the trade once accepted & the post\nPress the 'thanks' button on the post\nLeave a positive comment on our visitor page:`, ephemeral: messageFlag});
+    async execute({ client, inter, messageFlag }) {
+        inter.reply({content: `Follow these steps:\n\nSelect a post from the above list\nSend a trade request, and then leave a positive comment on the trade once accepted & the post\nPress the 'thanks' button on the post\nLeave a positive comment on our visitor page:`, ephemeral: messageFlag});
     }
 }
