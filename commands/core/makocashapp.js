ce255dc70ae8eefa8f3f4b8a02f215c448e4e14a
duhcy <35508017+duhcy@users.noreply.github.com>
2025-06-16 19:32:27 -0400

    Update makocashapp.js

--- commands/core/makocashapp.js
+++ commands/core/makocashapp.js
@@ -14,11 +14,8 @@ module.exports = {
 
     async execute({ client, inter, ephemeralStatus }) {
         const amount = inter.options.getNumber('amount');
-
-        const isDev = inter.user.id == client.config.app.DEV;
-        const isStaff = inter.member.roles.cache.has(client.config.opt.STAFFROLE);
-
-        if (!isDev && !isStaff) {
+        
+        if(!inter.member.user.id == client.config.app.DEV || !inter.member.roles.cache.has(client.config.opt.STAFFROLE)){
             return inter.reply({ content: "❌ You don't have permission to use this command.", ephemeral: true });
         }
 
