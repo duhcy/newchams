a6b47ca9633bb1ea4105b028abb8518077f19d06
duhcy <35508017+duhcy@users.noreply.github.com>
2025-06-20 17:49:16 -0400

    Update dma.js

--- commands/core/dma.js
+++ commands/core/dma.js
@@ -14,7 +14,7 @@ module.exports = {
                 { name: '💾 Flash Firmware', value: 'Flash or update firmware directly through the tool.' },
                 { name: '⚡ Speedtest', value: 'Benchmark the speed of your DMA card to ensure optimal performance.' }
             )
-            .setURL('https://cdn.discordapp.com/attachments/1254601397266288691/1373050521509953577/AIODMATool.rar?ex=684e9332&is=684d41b2&hm=e87d936b583765d70f73044808ddd221a8e2c0a9f7ada3c4ac24c03dfb79b28e&')
+            .setURL('https://github.com/kilmu1337/DMA-Tool/releases/')
             .setFooter({ text: 'Click the title or use the link above to get started!' })
             .setTimestamp();
         inter.reply({embeds: [embed], ephemeral: ephemeralStatus});
