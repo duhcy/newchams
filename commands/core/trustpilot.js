e8ca21a789e17e3b734213a926663badf9af561a
duhcy <35508017+duhcy@users.noreply.github.com>
2025-09-14 19:05:32 -0400

    Update trustpilot.js

--- commands/core/trustpilot.js
+++ commands/core/trustpilot.js
@@ -1,7 +1,7 @@
 const { EmbedBuilder } = require("discord.js");
 
 const reviewEmbed = new EmbedBuilder()
-  .setColor(0x00b67a) // Trustpilot green
+  .setColor(0x771BCB) // Trustpilot green
   .setTitle("Leave Us a Review ⭐")
   .setDescription("We value your feedback! Please consider leaving us a review on **[Trustpilot](https://www.trustpilot.com/review/chamscheats.com)**.")
   .setThumbnail("https://upload.wikimedia.org/wikipedia/commons/6/6e/Trustpilot_logo.png")
