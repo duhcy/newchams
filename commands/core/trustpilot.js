7e359d6d345dc15b672379c79f6cceaf443052f2
duhcy <35508017+duhcy@users.noreply.github.com>
2025-09-14 19:07:20 -0400

    Update trustpilot.js

--- commands/core/trustpilot.js
+++ commands/core/trustpilot.js
@@ -4,7 +4,7 @@ const reviewEmbed = new EmbedBuilder()
   .setColor(0x771BCB) // Trustpilot green
   .setTitle("Leave Us a Review ⭐")
   .setDescription("We value your feedback! Please consider leaving us a review on **[Trustpilot](https://www.trustpilot.com/review/chamscheats.com)**.")
-  .setThumbnail("https://upload.wikimedia.org/wikipedia/commons/6/6e/Trustpilot_logo.png")
+  .setThumbnail("https://www.vhv.rs/dpng/d/588-5885660_adding-trustpilot-image-next-to-logo-on-supply.png")
   .setFooter({ text: "Thank you for supporting us ❤️" });
 
 module.exports = {
