1f0640351bfc4aa9ecf3396baf79480882338218
duhcy <35508017+duhcy@users.noreply.github.com>
2026-06-03 10:52:07 -0400

    Update dmavideo.js

--- commands/core/dmavideo.js
+++ commands/core/dmavideo.js
@@ -1,20 +1,11 @@
-const { EmbedBuilder } = require('discord.js');
-
 module.exports = {
     name: 'dmavideo',
     description: 'DMA tutorial video',
 
     async execute({ client, inter, ephemeralStatus }) {
-        const embed = new EmbedBuilder()
-            .setColor(9448166)
-            .setTitle('🎥 DMA Tutorial Video')
-            .setDescription('Watch this video for DMA setup instructions:\n\nhttps://www.youtube.com/watch?v=-DxgOm_SRC4')
-            .setFooter({ 
-                text: 'ChamsCheats',
-                iconURL: 'https://cdn.discordapp.com/avatars/1377755363583201391/f61d35f7eb8e3c93af28bffe5ddd6971.webp?size=1024'
-            })
-            .setTimestamp();
-        
-        inter.reply({ embeds: [embed], ephemeral: ephemeralStatus });
+        inter.reply({ 
+            content: '🎥 **DMA Tutorial Video**\n\nWatch this video for DMA setup instructions:\n\nhttps://www.youtube.com/watch?v=-DxgOm_SRC4',
+            ephemeral: ephemeralStatus 
+        });
     }
 }
