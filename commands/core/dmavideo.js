896937ddf7c3b58149b6abba8896b350882bb51d
duhcy <35508017+duhcy@users.noreply.github.com>
2026-06-03 10:46:15 -0400

    Use direct video link in DMA embed
    Replace the embed's setURL and setImage with a simple clickable video link in the description and remove trailing whitespace. This simplifies the DMA tutorial embed by showing the YouTube URL directly instead of embedding the thumbnail/URL properties.

--- commands/core/dmavideo.js
+++ commands/core/dmavideo.js
@@ -6,11 +6,9 @@ module.exports = {
 
     async execute({ client, inter, ephemeralStatus }) {
         const embed = new EmbedBuilder()
-            .setColor(9448166) 
+            .setColor(9448166)
             .setTitle('🎥 DMA Tutorial Video')
-            .setDescription('Watch this comprehensive tutorial for DMA setup and usage instructions. Click the image or title to watch!')
-            .setURL('https://www.youtube.com/watch?v=-DxgOm_SRC4')
-            .setImage('https://img.youtube.com/vi/-DxgOm_SRC4/maxresdefault.jpg')
+            .setDescription('Watch this video for DMA setup instructions:\n\nhttps://www.youtube.com/watch?v=-DxgOm_SRC4')
             .setFooter({ 
                 text: 'ChamsCheats',
                 iconURL: 'https://cdn.discordapp.com/avatars/1377755363583201391/f61d35f7eb8e3c93af28bffe5ddd6971.webp?size=1024'
